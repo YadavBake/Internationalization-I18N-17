ff3a6edacd96fc314c3b5ac218e4126a0c497ea5
yadav Bakelal <bake2101992@gmail.com>
2024-03-16 12:08:19 +0530

    Add files via upload

---
+++ Internationalization [I18N] Part- 1Introduction.java
@@ -0,0 +1,25 @@
+
+											Internationalization [I18N] Part- 1||Introduction
+										=======================================================	
+
+------------------------------
+  Internationalization (I18N)
+------------------------------
+
+ -> The process of designing Web application in such way that which provide support for various contory and various
+	langauges and various currencys automatically without performing any change in the application, is called 
+	Internationalization (I18N).
+	
+ -> For example if the requiest is coming from india then the response should be in india people undarstandable form
+	and if teh requiest is coming from US then the response should be in US people undarstandable form.
+	
+ -> We can implement Internationalization by using the following three classes :
+	
+	1. Local.
+	
+	2. NumberFormat.
+	
+	3. DateFormat.
+					
+	
+ 										
